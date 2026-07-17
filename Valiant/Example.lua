80fd77929a1d494e8cce3121687436eb63357cb2
thunderstormik-elik <skividitailer@gmail.com>
2026-07-17 23:05:29 +0200

    Update library source URL in Example.lua

--- Valiant/Example.lua
+++ Valiant/Example.lua
@@ -1,4 +1,4 @@
-local library = loadstring(game:HttpGet('https://raw.githubusercontent.com/cypherdh/VanisUILIB/main/.gitignore'))()
+local library = loadstring(game:HttpGet('https://raw.githubusercontent.com/GhostDuckyy/UI-Libraries/refs/heads/main/Valiant/source.lua'))()
 
 local Window = library:CreateWindow("Name", "Version", 10044538000)
 
